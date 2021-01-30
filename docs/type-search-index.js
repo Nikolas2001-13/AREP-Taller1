add1214bb943c88e7af0446c352a0318f36eca18
Hogar <nikolas.bernal@mail.escuelaing.edu.co>
2021-01-29 23:49:00 -0500

    Modificacion de pom.xml y carpeta docs agregada

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.eci.arep","l":"AppTest"},{"p":"edu.eci.arep","l":"LinkedList"},{"p":"edu.eci.arep","l":"Main"},{"p":"edu.eci.arep","l":"Node"},{"p":"edu.eci.arep","l":"Operations"}]
